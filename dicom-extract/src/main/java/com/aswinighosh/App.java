12468332b51bf90b34de63e0e0b1604558bb3842
aswinigh <aswighosh@gmail.com>
2020-02-27 23:57:49 +0530

    Arguments used for input

--- dicom-extract/src/main/java/com/aswinighosh/App.java
+++ dicom-extract/src/main/java/com/aswinighosh/App.java
@@ -4,6 +4,7 @@ import ij.plugin.*;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.InputStream;
+import java.lang.IllegalArgumentException;
 /**
  * Hello world!
  *
@@ -14,14 +15,20 @@ public class App
 
     public static void main( String[] args )
     {   
-        String path = "/Users/aswinighosh/Downloads/DicomImages/cine_data/CAP_SCD0000101_MR__hrt_raw_20120813120605087_119.dcm";
+        
         //File f = new File();
         //InputStream is = new FileInputStream(f);
-        
-        DicomImage di = new DicomImage(path);
+        try{
+        if(args.length==0)
+            throw new IllegalArgumentException();
+        DicomImage di = new DicomImage(args[0]);
         di.showMetaData();
         
         di.showImage();
+        }
+        catch(Exception e){
+            System.err.println("ERROR:Dicom path not specified  "+e);
+        }
         
         
         
